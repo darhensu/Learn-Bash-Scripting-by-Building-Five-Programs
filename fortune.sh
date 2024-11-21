069fb3774d60729c8932dfe5407a3e70dab46272
Suhendar Aditya <mochammad.120450058@student.itera.ac.id>
2024-11-21 03:54:16 +0000

    Save progress: 1412.1

--- fortune.sh
+++ fortune.sh
@@ -7,12 +7,13 @@ echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 N=$(( RANDOM % 6 ))
 
-GET_FORTUNE() {
+function GET_FORTUNE() {
   if [[ ! $1 ]]
-  then 
+  then
     echo Ask a yes or no question:
-  fi
-  
+  else 
+   echo Try again. Make sure it ends with a question mark:
+   fi
   read QUESTION
 }
 
